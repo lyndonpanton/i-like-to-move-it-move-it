f25ab61a79ac3334bc7d65f0d440cc59245d26f4
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-24 22:31:11 +0000

    Completed the problem

--- Exercise5/Assets/scripts/BallSpawner.cs
+++ Exercise5/Assets/scripts/BallSpawner.cs
@@ -17,15 +17,35 @@ public class BallSpawner : MonoBehaviour
     GameObject ballBlue;
 
     Timer spawnTimer;
+
+    int duration = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTimer = GetComponent<Timer>();
+        spawnTimer.Duration = duration;
+        spawnTimer.Run();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnTimer.Finished)
+        {
+            spawnTimer.Duration = duration;
+            spawnTimer.Run();
+
+            GameObject[] balls =
+            {
+                ball,
+                ballGreen,
+                ballBlue,
+            };
+
+            int option = Random.Range(0, balls.Length);
+
+            GameObject chosenBall = Instantiate(balls[option]) as GameObject;
+            //Instantiate(chosenBall);
+        }
     }
 }
