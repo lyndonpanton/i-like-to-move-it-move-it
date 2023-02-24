e8d9920fb7b5fb362c71e4b596046abbd28ad5fe
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-24 22:15:16 +0000

    Created ball spawner script

---
+++ Exercise5/Assets/scripts/BallSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A ball spawner
+/// </summary>
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ball;
+
+    [SerializeField]
+    GameObject ballGreen;
+
+    [SerializeField]
+    GameObject ballBlue;
+
+    Timer spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
